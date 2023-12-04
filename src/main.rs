384c3e8aacb030bf06956094b386895afdfe0816
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-12-04 18:16:43 +0100

    feat: add basic functionality

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use anyhow::{Context, Result};
+use headless_chrome::browser::LaunchOptions;
+use headless_chrome::types::PrintToPdfOptions;
+use headless_chrome::Browser;
+use parser::Rule;
+use pest::Parser;
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+use std::process::exit;
+use tempfile::tempdir;
+
+mod parser;
+
+const FACTOR_MM_TO_INCHES: f64 = 25.4;
+
+fn main() -> Result<()> {
+    let arguments: Vec<String> = env::args().collect();
+
+    if arguments.len() != 3 {
+        println!("Usage: {} <input.twml> <output.pdf>", arguments[0]);
+        exit(22);
+    }
+
+    let document =
+        fs::read_to_string(&arguments[1]).context("Failed to read the input document")?;
+    let pairs = parser::DocumentParser::parse(Rule::document, &document)
+        .context("Failed to interpret the provided document")?;
+    let declarations = parser::DocumentParser::get_declarations(pairs.clone())
+        .context("Failed to parse the declarations")?;
+    let html = parser::DocumentParser::generate_html(&declarations, pairs)
+        .context("Failed to generate html code")?;
+
+    let temporary_dir = tempdir().context("Failed to create a temporary directory")?;
+    let index_path = setup_rendering_env(&declarations, temporary_dir.path(), &html)?;
+    export_pdf(&declarations, &index_path, &arguments[2])?;
+
+    Ok(())
+}
+
+fn setup_rendering_env(
+    declarations: &parser::Declarations,
+    temporary_dir: &Path,
+    html: &str,
+) -> Result<String> {
+    let index_path = temporary_dir.join("index.html");
+    let mut index_file = fs::File::create(&index_path)?;
+    write!(index_file, "{}", html)?;
+
+    for file in &declarations.include {
+        fs::copy(
+            file,
+            &temporary_dir.join(Path::new(file).file_name().unwrap()),
+        )
+        .context(format!("Failed to include '{}'", file))?;
+    }
+
+    Ok(index_path.display().to_string())
+}
+
+fn export_pdf(
+    declarations: &parser::Declarations,
+    index_path: &str,
+    output_pdf_path: &str,
+) -> Result<()> {
+    let browser = Browser::new(LaunchOptions::default())
+        .context("Failed to initialize a headless_chrome Browser instance")?;
+    let tab = browser.new_tab()?;
+    let local_pdf = tab
+        .navigate_to(&format!("file://{}", index_path))?
+        .wait_until_navigated()?
+        .print_to_pdf(Some(PrintToPdfOptions {
+            paper_width: Some(
+                declarations.page_width_mm.unwrap_or(210) as f64 / FACTOR_MM_TO_INCHES,
+            ),
+            paper_height: Some(
+                declarations.page_height_mm.unwrap_or(297) as f64 / FACTOR_MM_TO_INCHES,
+            ),
+            margin_top: Some(0.0),
+            margin_bottom: Some(0.0),
+            margin_left: Some(0.0),
+            margin_right: Some(0.0),
+            ..PrintToPdfOptions::default()
+        }))
+        .context("Failed to render the pdf")?;
+
+    fs::write(output_pdf_path, local_pdf).context("Failed to write the output pdf")?;
+
+    Ok(())
+}
---
+++ src/parser.rs
@@ -0,0 +1,240 @@
+use anyhow::{anyhow, Context, Result};
+use dyn_fmt::AsStrFormatExt;
+use indoc::indoc;
+use pest::iterators::{Pair, Pairs};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct DocumentParser;
+
+#[derive(Debug)]
+pub struct Declarations {
+    pub include: Vec<String>,
+    pub page_width_mm: Option<u64>,
+    pub page_height_mm: Option<u64>,
+}
+
+impl DocumentParser {
+    pub fn get_declarations(mut pairs: Pairs<Rule>) -> Result<Declarations> {
+        let mut include: Vec<String> = Vec::new();
+        let mut page_width_mm: Option<u64> = None;
+        let mut page_height_mm: Option<u64> = None;
+
+        for pair in pairs.next().unwrap().into_inner() {
+            match pair.as_rule() {
+                Rule::element => {}
+                Rule::EOI => {}
+                Rule::declarations => {
+                    for declaration_pair in pair.into_inner() {
+                        if declaration_pair.as_rule() != Rule::declaration {
+                            return Err(anyhow!("Unexpected declarations rule"));
+                        }
+
+                        let mut iterator = declaration_pair.into_inner();
+                        let declaration_key_pair = iterator.next().unwrap();
+                        let declaration_value_pair = iterator.next().unwrap();
+
+                        if declaration_key_pair.as_rule() != Rule::declaration_key
+                            || declaration_value_pair.as_rule() != Rule::declaration_value
+                        {
+                            return Err(anyhow!("Declaration pairs out of order"));
+                        }
+
+                        let declaration_key = declaration_key_pair.as_span().as_str();
+                        let declaration_value = declaration_value_pair.as_span().as_str();
+
+                        match declaration_key {
+                            "include" => include.push(declaration_value.to_string()),
+                            "page-width" => {
+                                page_width_mm = Some(
+                                    declaration_value
+                                        .parse::<u64>()
+                                        .context("The page-width value is not of u64 value")?,
+                                )
+                            }
+                            "page-height" => {
+                                page_height_mm = Some(
+                                    declaration_value
+                                        .parse::<u64>()
+                                        .context("The page-height value is not of u64 value")?,
+                                )
+                            }
+                            _ => {
+                                return Err(anyhow!(format!(
+                                    "The declaration key '{}' is unexpected",
+                                    declaration_key
+                                )))
+                            }
+                        }
+                    }
+                }
+                _ => return Err(anyhow!(format!("Unexpected document rule: {:?}", pair))),
+            }
+        }
+
+        Ok(Declarations {
+            include,
+            page_width_mm,
+            page_height_mm,
+        })
+    }
+
+    pub fn generate_html(declarations: &Declarations, pairs: Pairs<Rule>) -> Result<String> {
+        let generated_html = Self::generate_html_document(pairs)?;
+
+        let mut warnings: Vec<railwind::warning::Warning> = Vec::new();
+        let generated_css = railwind::parse_to_string(
+            railwind::Source::String(generated_html.clone(), railwind::CollectionOptions::Html),
+            false,
+            &mut warnings,
+        )
+        .replace('\n', "")
+        .replace("    ", "");
+
+        let html = indoc! {"
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <style>
+                .page {{ width: {}mm; height: {}mm; }}
+                {}
+              </style>
+            </head>
+            <body>
+              {}
+            </body>
+            </html>
+        "}
+        .replace('\n', "")
+        .replace("  ", "")
+        .format(&[
+            // default: A4
+            declarations.page_width_mm.unwrap_or(210).to_string(),
+            declarations.page_height_mm.unwrap_or(297).to_string(),
+            generated_css,
+            generated_html,
+        ]);
+
+        Ok(html)
+    }
+
+    fn generate_html_document(mut pairs: Pairs<Rule>) -> Result<String> {
+        let mut html = String::new();
+
+        for pair in pairs.next().unwrap().into_inner() {
+            match pair.as_rule() {
+                Rule::element => html.push_str(&Self::generate_html_element(pair)?),
+                Rule::EOI => {}
+                Rule::declarations => {}
+                _ => return Err(anyhow!(format!("Unexpected document rule: {:?}", pair))),
+            }
+        }
+
+        Ok(html)
+    }
+
+    fn generate_html_children(pairs: Pairs<Rule>) -> Result<String> {
+        let mut html = String::new();
+
+        for pair in pairs {
+            match pair.as_rule() {
+                Rule::block => {
+                    let mut newline_needed = false;
+
+                    for class_pair in pair.into_inner() {
+                        if class_pair.as_rule() != Rule::block_content {
+                            return Err(anyhow!("Unexpected block rule"));
+                        }
+
+                        if newline_needed {
+                            html.push_str("<br>");
+                        }
+
+                        html.push_str(class_pair.as_span().as_str());
+                        newline_needed = true;
+                    }
+                }
+                Rule::element => {
+                    html.push_str(&Self::generate_html_element(pair)?);
+                }
+                _ => return Err(anyhow!(format!("Unexpected children rule: {:?}", pair))),
+            }
+        }
+
+        Ok(html)
+    }
+
+    fn generate_html_element(element_pair: Pair<Rule>) -> Result<String> {
+        let mut html = String::from("<");
+        let mut element_name = String::new();
+
+        for pair in element_pair.into_inner() {
+            match pair.as_rule() {
+                Rule::element_name => {
+                    element_name = pair.as_span().as_str().to_owned();
+                    html.push_str(&element_name);
+                }
+                Rule::element_classes => {
+                    html.push_str(" class=\"");
+
+                    let mut seperator_needed = false;
+
+                    for class_pair in pair.into_inner() {
+                        if class_pair.as_rule() != Rule::element_class_name {
+                            return Err(anyhow!("Unexpected element_classes rule"));
+                        }
+
+                        if seperator_needed {
+                            html.push(' ');
+                        }
+
+                        html.push_str(class_pair.as_span().as_str());
+                        seperator_needed = true;
+                    }
+                    html.push('"');
+                }
+                Rule::element_attributes => {
+                    for attribute_pair in pair.into_inner() {
+                        if attribute_pair.as_rule() != Rule::element_attribute {
+                            return Err(anyhow!("Unexpected element_attributes rule"));
+                        }
+
+                        let mut iterator = attribute_pair.into_inner();
+                        let attribute_key_pair = iterator.next().unwrap();
+                        let attribute_value_pair = iterator.next().unwrap();
+
+                        if attribute_key_pair.as_rule() != Rule::element_attribute_key
+                            || attribute_value_pair.as_rule() != Rule::element_attribute_value
+                        {
+                            return Err(anyhow!("Attribute pairs out of order"));
+                        }
+
+                        html.push(' ');
+                        html.push_str(attribute_key_pair.as_span().as_str());
+                        html.push_str("=\"");
+                        html.push_str(
+                            &attribute_value_pair
+                                .as_span()
+                                .as_str()
+                                .replace("\\\"", "&quot;"),
+                        );
+                        html.push('"');
+                    }
+                }
+                Rule::element_content => {
+                    let content = pair.as_span().as_str();
+                    html.push_str(&format!(">{}</{}>", content, &element_name))
+                }
+                Rule::children => {
+                    html.push('>');
+                    html.push_str(&Self::generate_html_children(pair.into_inner())?);
+                    html.push_str(&format!("</{}>", &element_name))
+                }
+                _ => return Err(anyhow!(format!("Unexpected element rule: {:?}", pair))),
+            }
+        }
+
+        Ok(html)
+    }
+}
